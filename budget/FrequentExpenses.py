2fbadb3c997d6a792cba0947b928da2da51299e0
shinafifloo <effati.afshin@gmail.com>
2023-01-13 13:58:40 -0500

    p3

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -7,6 +7,7 @@ expenses.read_expenses("data/spending_data.csv")
 
 spending_categories = []
 for expense in expenses.list:
+    # print(expense)
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
